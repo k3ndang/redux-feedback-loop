393aa7e6fa9224852271c90deb43d43963eeb94b
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-01-23 10:05:38 -0600

    created checkbox to flag for review that toggles

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import IconCheckboxes from "../IconCheckboxes/IconCheckboxes";
 
 function Admin() {
   let [resultList, setResultList] = useState([]);
@@ -52,6 +58,10 @@ function Admin() {
     });
   };
 
+  const review = (check) => {
+    console.log("Review", check);
+  };
+
   return (
     <>
       <header className="Admin-header">
@@ -60,6 +70,7 @@ function Admin() {
       <table>
         <tbody>
           <tr>
+            <th>Review</th>
             <th>Feeling</th>
             <th>Comprehension</th>
             <th>Support</th>
@@ -69,6 +80,9 @@ function Admin() {
 
           {resultList.map((result) => (
             <tr key={result.id}>
+              <td onClick={() => review(result.id)}>
+                <IconCheckboxes />
+              </td>
               <td>{result.feeling}</td>
               <td>{result.understanding}</td>
               <td>{result.support}</td>
